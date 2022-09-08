48ed6ad0b9a55d1910298058d6f1fd91c12e8918
mbayerworking <mbayer.work@gmail.com>
2022-09-08 14:58:46 -0700

    Pseudo code for Get_Max_Profit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Apple Stock Question");
+
+/// <summary>
+/// the indices are the time in minutes past trade opening time, which
+/// was 9:30 local time
+/// the values are the price (in USD) of one share of Apple stock
+/// at that time
+/// </summary>
+var stock_prices = [10,7,5,8,11,9];
+
+
+public int Get_Max_Profit()
+{
+    // loop from 0 to list length -1
+    // Start from first index
+    // traverse whole tree looking for values higher than current node
+    // find comparative max in remaining nodes
+    // find diff of max and index
+    // save profit.
+    
+}
