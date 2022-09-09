be9c8ac545aba8e023c2ab65fef9696caacd604b
mbayerworking <mbayer.work@gmail.com>
2022-09-08 19:22:21 -0700

    Clean solution in nlogn

--- Program.cs
+++ Program.cs
@@ -6,29 +6,20 @@
 /// the values are the price (in USD) of one share of Apple stock
 /// at that time
 /// </summary>
-int[] stock_prices = new int[]{10,7,5,8,11,9};
-Console.WriteLine(Get_Max_Profit(stock_prices));
+//int[] stock_prices = new int[]{10,7,5,8,11,9};
+int[] stock_prices = new int[]{7,4,3,2,1};
+Console.WriteLine("Max profit is: {0}", Get_Max_Profit(stock_prices));
 
 int Get_Max_Profit(int[] stock_prices)
 {
-    var profit = 0;
-    // loop from 0 to list length -1
-    // traverse whole list looking for values higher than current node
-    // Start from first index
+    var profit = stock_prices[1] - stock_prices[0];
     for (int i = 0; i < stock_prices.Length-1; ++i)
     {
-        Console.WriteLine("i = {0}, stock_price[i] = {1}",i,stock_prices[i]);
         int node = stock_prices[i];
-        //int lm = Get_Local_Max(stock_prices);
-        int lm = Get_Local_Max_Recursive(stock_prices);
-        Console.WriteLine(lm);
-        if (lm - node > profit)
-        {
-            profit = lm - node;
-        }
+        int j = i+1;
+        int lm = Get_Local_Max_Recursive(stock_prices[j..]);
+        profit = lm - node;
     }
-        
-    // save profit.
     return profit;
 }
 
