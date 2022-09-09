394279593f35ea061b55c68bb471746b088c5572
mbayerworking <mbayer.work@gmail.com>
2022-09-08 18:35:51 -0700

    Change of tactic, was making to complicated

--- Program.cs
+++ Program.cs
@@ -6,16 +6,32 @@
 /// the values are the price (in USD) of one share of Apple stock
 /// at that time
 /// </summary>
-var stock_prices = [10,7,5,8,11,9];
+int[] stock_prices = new int[]{10,7,5,8,11,9};
+Console.WriteLine(Get_Max_Profit(stock_prices));
 
-
-public int Get_Max_Profit()
+int Get_Max_Profit(int[] stock_prices)
 {
+    var profit = 0;
     // loop from 0 to list length -1
+    // traverse whole list looking for values higher than current node
     // Start from first index
-    // traverse whole tree looking for values higher than current node
+    for (int i = 0; i < stock_prices.Length-1; ++i)
+    {
+        Console.WriteLine("i = {0}, stock_price[i] = {1}",i,stock_prices[i]);
+        int node = stock_prices[i];
+        int localMax = stock_prices[i+1];
+        
+        
+
     // find comparative max in remaining nodes
-    // find diff of max and index
+        if(localMax - node > profit)
+        {
+        // find diff of max and index
+            profit = localMax - node;
+        }
+    }
+        
     // save profit.
-    
+    return profit;
 }
+
