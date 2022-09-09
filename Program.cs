635fe37cb720d8b2e129498cf3246939f41588e6
mbayerworking <mbayer.work@gmail.com>
2022-09-08 19:46:44 -0700

    Implemented a greedy approach, n time, const space

--- Program.cs
+++ Program.cs
@@ -6,9 +6,10 @@
 /// the values are the price (in USD) of one share of Apple stock
 /// at that time
 /// </summary>
-//int[] stock_prices = new int[]{10,7,5,8,11,9};
-int[] stock_prices = new int[]{7,4,3,2,1};
+int[] stock_prices = new int[]{10,7,5,8,11,9};
+//int[] stock_prices = new int[]{7,4,3,2,1};
 Console.WriteLine("Max profit is: {0}", Get_Max_Profit(stock_prices));
+Console.WriteLine("Max profit is: {0}", Get_Max_Profit_Greedy(stock_prices));
 
 int Get_Max_Profit(int[] stock_prices)
 {
@@ -19,6 +20,33 @@ int Get_Max_Profit(int[] stock_prices)
         int j = i+1;
         int lm = Get_Local_Max_Recursive(stock_prices[j..]);
         profit = lm - node;
+    }
+    return profit;
+}
+
+int Get_Max_Profit_Greedy(int[] stock_prices)
+{
+    int minSoFar = stock_prices[0];
+    Console.WriteLine("Min So Far {0}", minSoFar);
+    int profit = stock_prices[1] - stock_prices[0];
+    Console.WriteLine("Profit So Far {0}", profit);
+    for(int i = 1; i < stock_prices.Length; ++i)
+    {
+        Console.WriteLine("Iteration {0}", i);
+        Console.WriteLine("stock_prices[i] = {0}", stock_prices[i]);
+        if(profit < (stock_prices[i]-minSoFar))
+        {
+            profit = (stock_prices[i]-minSoFar);
+            Console.WriteLine("Profit So Far {0}", profit);
+
+
+        }
+        if(minSoFar > stock_prices[i]){
+
+            minSoFar = stock_prices[i];
+            Console.WriteLine("Min So Far {0}", minSoFar);
+        }
+
     }
     return profit;
 }
